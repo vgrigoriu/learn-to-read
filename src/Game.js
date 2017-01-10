2195fe973960ae994d633098a6579c332be8f1fe
Victor Grigoriu <vgrigoriu@gmail.com>
2017-01-10 22:22:58 +0200

    Add banana image to src

--- src/Game.js
+++ src/Game.js
@@ -1,14 +1,11 @@
 import React, { Component } from 'react';
-
-const storkImage = 'http://www.ciconia.ro/allo.jpg';
-const ballonImage = 'http://www.balon.ro/31074-home_default/balon-latex-pale-blue-11-inch-28-cm-qualatex-43762.jpg';
-const bananaImage = 'http://www.arrms.org/media/blogs/blog/banana.jpg?mtime=1417645870'
+import banana from './images/banana.jpg';
 
 class Game extends Component {
     render() {
         return (
             <div>
-                <img src={bananaImage} />
+                <img src={banana} alt='banană' />
                 <div onClick={() => alert('balon')}>BALON</div>
                 <div onClick={() => alert('barză')}>BARZĂ</div>
                 <div onClick={() => alert('banană')}>BANANĂ</div>
