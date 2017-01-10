f15c6342af5d6d7ae870d4bc320c59d295f70263
Victor Grigoriu <vgrigoriu@gmail.com>
2017-01-10 22:08:26 +0200

    Display a banana and three words

--- src/Game.js
+++ src/Game.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
 
+const storkImage = 'http://www.ciconia.ro/allo.jpg';
+const ballonImage = 'http://www.balon.ro/31074-home_default/balon-latex-pale-blue-11-inch-28-cm-qualatex-43762.jpg';
+const bananaImage = 'http://www.arrms.org/media/blogs/blog/banana.jpg?mtime=1417645870'
+
 class Game extends Component {
     render() {
-        return <div>game goes here</div>;
+        return (
+            <div>
+                <img src={bananaImage} />
+                <div onClick={() => alert('balon')}>BALON</div>
+                <div onClick={() => alert('barză')}>BARZĂ</div>
+                <div onClick={() => alert('banană')}>BANANĂ</div>
+            </div>
+        );
     }
 }
 
