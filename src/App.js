90c4ac9032611cf106f0d7b61b8a69554ed9fda6
Victor Grigoriu <vgrigoriu@gmail.com>
2017-01-10 21:59:55 +0200

    Add Game.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+// @flow
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Game from './Game';
 
 class App extends Component {
   render() {
@@ -8,10 +10,10 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Learn to Read</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <Game />
         </p>
       </div>
     );
---
+++ src/Game.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Game extends Component {
+    render() {
+        return <div>game goes here</div>;
+    }
+}
+
+export default Game;
