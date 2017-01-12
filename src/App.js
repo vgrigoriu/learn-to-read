4ac37d5a222966e31e537c932f8583b1b9bf73c2
Victor Grigoriu <vgrigoriu@gmail.com>
2017-01-12 08:39:19 +0200

    Reduce header height

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 // @flow
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Game from './Game';
 
@@ -9,7 +8,6 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h2>Learn to Read</h2>
         </div>
         <div className="App-intro">
