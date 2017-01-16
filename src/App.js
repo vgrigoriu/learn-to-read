630754972465b0c41d91f76b652eaa81a28e8aec
Victor Grigoriu <vgrigoriu@gmail.com>
2017-01-16 19:22:51 +0200

    Remove useless header

--- src/App.js
+++ src/App.js
@@ -7,9 +7,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <h2>Learn to Read</h2>
-        </div>
         <div className="App-intro">
           <Game />
         </div>
